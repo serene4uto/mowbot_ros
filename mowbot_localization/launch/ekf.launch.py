854f20b5ae9afe9384188d7f4d6de7462dabcca6
zeki <nguyenhatrung411@gmail.com>
2024-11-18 10:30:07 +0000

    Update EKF launch configuration to use 'config' directory and add EKF parameters

--- mowbot_localization/launch/ekf.launch.py
+++ mowbot_localization/launch/ekf.launch.py
@@ -2,9 +2,6 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
-import yaml
-from launch.substitutions import EnvironmentVariable
-import pathlib
 import launch.actions
 from launch.actions import DeclareLaunchArgument
 
@@ -25,7 +22,7 @@ def generate_launch_description():
             name='ekf_filter_node',
             output='screen',
             parameters=[
-                os.path.join(get_package_share_directory("mowbot_localization"), 'param', 'ekf_params.yaml'),
+                os.path.join(get_package_share_directory("mowbot_localization"), 'config', 'ekf_params.yaml'),
                 {"use_sim_time": use_sim_time},
             ],
         ),
